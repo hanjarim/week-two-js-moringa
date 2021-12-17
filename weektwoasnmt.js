7d626c1b850c32a14a786487f604149eb9e5d96f
moses hanjari <mkubo@safaricom.co.ke>
2021-12-17 13:10:29 +0300

    new lines

--- weektwoasnmt.js
+++ weektwoasnmt.js
@@ -4,7 +4,7 @@ const akanNameCalculator = (CC, YY, MM, DD, gender) => {
     const calculationResult = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
 
     //console.log(calculationResult)
-    let wholeNumber = Number(calculationResult.toFixed())
+    //let wholeNumber = Number(calculationResult.toFixed())
 
     const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
